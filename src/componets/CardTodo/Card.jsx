88022f002413776e5a7c6a8cb221a30a10abd84d
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-01-28 17:22:10 +0330

    finish card

--- src/componets/CardTodo/Card.jsx
+++ src/componets/CardTodo/Card.jsx
@@ -1,11 +1,14 @@
 import "./card.css"
 
-const Card = ()=>{
+const Card = ({TodoItem})=>{
 
     return(
-        <li>
-            <h2>Title</h2>
-            <p> Describe Todo </p>
+        <li className="cardTodo-container">
+            <div className="todo-content">
+                
+            <h2>{TodoItem.title}</h2>
+            <p> {TodoItem.describe} </p>
+            </div>
             <div className="card-btn" >
                 <button className="btn-action">Delete</button>
                 <button className="btn-action">Edite</button>
