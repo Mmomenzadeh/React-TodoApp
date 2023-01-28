639f14c085cc8b24ee6cee715904f85d5ba2f550
Mmomenzadeh <m.momenzadeh1999@gmail.com>
2023-01-28 17:21:23 +0330

    finish form

--- src/componets/Form/Form.jsx
+++ src/componets/Form/Form.jsx
@@ -1,14 +1,52 @@
-import "./form.css"
+import React, { useState } from "react";
+import "./form.css";
+import { v4 as uuidv4 } from "uuid";
 
-const Form = ()=>{
+const Form = ({ TodoList, setTodoList }) => {
+  const initialFormValue = {
+    title: "",
+    describe: "",
+    id: "",
+  };
+  const [formValue, setFormValue] = useState(initialFormValue);
 
-    return(
-        <form className="form-container">
-            <input type="text" />
-            <input type="text" />
-            <button>Submit</button>
-        </form>
-    )
-}
+  const CreateTodoHandler = (e) => {
+    e.preventDefault();
+    let newTodo = {
+      title: formValue.title,
+      describe: formValue.describe,
+      id: uuidv4(),
+    };
+    setTodoList([...TodoList, newTodo]);
+   
+  };
 
-export default Form
+  return (
+    <form className="form-container" onSubmit={CreateTodoHandler}>
+      
+     <div className="input-container">
+     <div className="input-form">
+      <label> Title</label>
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={(e) => setFormValue({ ...formValue, title: e.target.value })}
+      />
+      </div>
+      <div className="input-form">
+      <label>Describe</label>
+      <input
+        type="text"
+        placeholder="Describe"
+        onChange={(e) =>
+          setFormValue({ ...formValue, describe: e.target.value })
+        }
+      />
+      </div>
+     </div>
+      <button className="btn-form">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
